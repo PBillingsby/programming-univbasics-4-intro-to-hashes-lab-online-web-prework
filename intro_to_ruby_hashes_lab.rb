184685e82e97af73a12c7d4e2e951a8f2cdc6981
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-03 18:08:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -76,7 +76,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  new = { :k => key, :v => value }
+  new = { :key => value }
   new
 end
 
