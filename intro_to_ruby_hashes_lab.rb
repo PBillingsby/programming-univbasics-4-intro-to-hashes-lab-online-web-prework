256684177e71a810e153e2e9007274bf6157a88e
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-03 18:16:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -77,6 +77,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   new = { :key => }
+  new
 end
 
 def read_from_hash(hash, key)
